830e4f270dd5a63cd4640565cc7ffd9be3361d2c
Crazy_shrabin <shrabin8@gmail.com>
2026-05-12 00:35:12 +0545

    1st day

---
+++ day1.py
@@ -0,0 +1,76 @@
+# name = " bibek"
+# age = 20
+# price = 100.00
+# total = 100+20
+
+# # print (" my namme is", name, "my age is", age, " the price is", price, "the total is", total)
+
+# # print(type(name))
+# # print(type(age))
+# # print(type(price))
+# # print(type(total))
+
+# name1 = "bibek"
+# name2 = 'bibek'
+# name3 = """bibek"""
+
+# # print(name1)
+# # print(name2)
+# # print(name3)
+
+
+# age1 = 23
+# old = False
+# a = None
+
+# # print(type(old))
+# # print(type(age1))
+# # print(type(a))
+
+# a = 5
+# b = 2
+# sum = a + b
+# print (sum)
+
+# #Arithmetic Operators
+# print (a - b) #subtraction
+# print (a * b) #multiplication
+# print (a / b) #division
+# print (a + b) #addition
+# print (a % b) #remainder
+# print (a ** b) #a^b
+
+# #relational operators
+# print(a == b) #equal to
+# print(a != b) #not equal to
+# print(a > b) #  a is greaater than b
+# print(a < b) #  b is greater than a
+# print(a >= b) #  a is greater than or equal to b
+# print(a <= b) #  b is greater than or equal to a
+
+# #assignment operators
+# num = 10
+# num = num + 10 #10 + 10 = 20
+# print (num)
+# print (not a > b)
+
+# #logical operators
+# print (not a > b) #not operator
+# print (and a > b and a < 20) #and operator
+# print (or a > b or a < 20) #or operator
+
+
+#type conversion
+# a =int ("2")
+# b = 3.5
+ 
+# print (a +b)
+# print (type(a))
+
+# name =input("enter your age: ")
+# print("you entered", name )
+
+# num1= int(input("enter the area of side: "))
+
+# area=(num1*num1)
+# print("The areas:",area)
