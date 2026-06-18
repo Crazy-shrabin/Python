a0a3c832d81ca8dcc85ae269d614b5ae5dc6fce1
Crazy_shrabin <shrabin8@gmail.com>
2026-06-18 18:08:49 +0545

    Add files via upload

---
+++ day 2.py
@@ -0,0 +1,53 @@
+a = 5
+b = 10
+c = 20
+d = 40
+
+# a = str(a)
+
+
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a % b)
+# print(a ** b) 
+
+
+# print(a==b)
+# print(a!=b)
+# print(a >= b)
+# print(a > b)
+# print(a <= b)
+# print(a < b)
+
+# num = 10
+# num = 10
+# num = num + 10  #10+10 --> 20
+# num += 10 (same meaning with the upper sentence)
+
+# num = num - 10  #10-10 --> 0
+# num -= 10 (same meaning with the upper sentence)
+
+# num = num * 10  #10*10 --> 100
+# num *= 10 (same meaning with the upper sentence)
+
+# num = num / 10  #10/10 --> 1
+# num /= 10 (same meaning with the upper sentence)
+
+# num = num % 10  #10%10 --> 0
+# num %= 10 (same meaning with the upper sentence) (modular/reminder)
+
+# num = num ** 10  #10**10 --> 10000000000
+# num **= 10 (same meaning with the upper sentence) (power)
+
+# print(not(a>b))
+# print(a<b)
+
+# Val1 = True
+# Val2 = False
+# print("OR operator", Val1 or Val2)
+# print("AND operator", Val1 and Val2)
+# print("OR operator", (a==b) or (a>b))
+
+# print(type(a))
