b0f4da7dd8baac26aa65f68af0d25e2ed7099b2f
Crazy_shrabin <shrabin8@gmail.com>
2026-06-30 19:16:44 +0545

    Create pratice.py

---
+++ pratice.py
@@ -0,0 +1,38 @@
+# dict = {
+#     'table' :["a piece of furniture"," list of facts and figures"],
+#     "cat" : "a small animal"
+# }
+
+# print(dict)
+
+
+
+
+
+
+
+# subjects = {
+#     "python", "java", "c++", "python", "javascript", "java", "python", "java", "c++", "c"
+# }
+
+# print(len(subjects))
+
+
+
+
+
+
+
+
+# marks = {}
+# p = int(input("enter the mark of phy:"))
+# marks.update({"phy": p})
+# m = int(input("enter the mark of mgmt:"))
+# marks.update({"mgmt": m})
+# c = int(input("enter the mark of che:"))
+# marks.update({"che": c})
+
+# print(marks)
+
+
+
