5252941648d9d587df4b76fd4a51ba099cbc5091
Crazy_shrabin <shrabin8@gmail.com>
2026-06-30 22:01:21 +0545

    Create lecture5.py

---
+++ lecture5.py
@@ -0,0 +1,55 @@
+# count = 1
+# while count <=5 :
+#     print("hello")
+#     count = count + 1 #count +=1
+
+# print(count)
+
+
+
+
+
+
+# count = 10
+# while count >= 0:
+#     print ("laaa")
+#     count -= 1
+
+
+
+
+
+
+
+# a = 1
+# while a <= 100:
+#     print(a)
+#     a += 1
+
+
+
+
+
+
+
+# b = 100
+# while b >= 1:
+#     print(b)
+#     b -= 1
+
+
+
+
+
+
+
+
+# d = int(input("enter the number: "))
+# c = 1
+# while c <= 10:
+#     print( d* c)
+#     c += 1
+
+
+
+
