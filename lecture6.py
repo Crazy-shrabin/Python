9466c6ae93042400a66e884b5c0bda2f42dc6b53
Crazy_shrabin <shrabin8@gmail.com>
2026-07-03 23:04:11 +0545

    Update lecture6.py

---
+++ lecture6.py
@@ -0,0 +1,16 @@
+def calc_sum(a, b):
+    return a + b
+
+
+def calc_average(a, b, c):
+    sum = int(a) + int(b) + int(c)
+    avg = sum / 3
+    print(avg)
+
+
+def print_hello():
+    print("hello mummy!!")
+
+
+
+calc_average(5, 213123, 342)
