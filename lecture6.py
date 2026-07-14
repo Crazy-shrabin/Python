49805a00045a31a4095436c0ee9be5e6a2fafc60
Crazy_shrabin <shrabin8@gmail.com>
2026-07-14 22:42:07 +0545

    ew

--- lecture6.py
+++ lecture6.py
@@ -1,5 +1,5 @@
-# cities = ["ktm", "pkr", "ctwn", "brtn", "illam"]
-# famous = ["temple","nature", "market", "deal", "teas"]
+cities = ["ktm", "pkr", "ctwn", "brtn", "illam"]
+famous = ["temple","nature", "market", "deal", "teas"]
 
 
 # def print_len(list):
@@ -34,11 +34,45 @@
 
 # converter(15)
 
+# recusive function
+# def show (n):
+#     if (n == 0):
+#         return
+#     print(n)
+#     show(n - 1)
 
-def show (n):
-    if (n == 0):
-        return
-    print(n)
-    show(n - 1)
+# show(55)
 
-show(55)
+def calc_sum(a, b):
+    return a + b
+def print_hello ():
+    print("hello")
+def calc_avg(a, b, c):
+    sum = a + b + c
+    div = sum/3
+    print(div)
+    return div
+def len_cities(list):
+    print(len(list))
+def single_line(list):
+    print(list)
+    return list
+def print_list(list):
+    for item in list:
+        print(item, end = " ")
+def cal_fact(n):
+    fact = 1
+    for i in range(1, n+1):
+        fact *= i
+    print(fact)        
+
+
+
+
+
+
+def usd_to_npr(n):
+    a = 153.82 * n
+    print("The total money conversion amount is: ", a) 
+
+usd_to_npr(20)
