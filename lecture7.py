53ecb1ba3af486370015af447692e4b9290da288
Crazy_shrabin <shrabin8@gmail.com>
2026-07-16 23:54:05 +0545

    gg

--- lecture7.py
+++ lecture7.py
@@ -1,9 +1,21 @@
-# import os
-f = open("demo.txt", "r")
+# f = open("demo.txt", "a")
 
-data = f.readline()
-print(data)
-data2 = f.readline()
-print(data2)
+# data = f.write("I am gonna fuck every one of you.")
+# print(data)
 
-f.close()
+# f.close()
+
+# f = open("demo.txt", "r+")
+# f.write("boka")
+
+# print(f.read())
+# f.close()
+
+
+#with syntax
+# with open("demo.txt", "r") as a:
+#     element = a.read()
+#     print(element)
+
+import os
+os.remove("demo.txt")
