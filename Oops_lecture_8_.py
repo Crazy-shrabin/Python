1ee823da0e2b7f9323337d3f612c07563f821529
Crazy_shrabin <shrabin8@gmail.com>
2026-07-18 18:42:05 +0545

    ggggg

--- Oops_lecture_8_.py
+++ Oops_lecture_8_.py
@@ -89,3 +89,57 @@ S1.welcome()
 print(S1.get_address())
 
 
+#--------------------------Satic Method----------------------
+# example:
+# class Student:
+#     @staticmethod
+#     def collage():
+#         print("ABC Collage")
+
+
+
+
+
+
+
+#Abstraction --- hiding the implementation details of a class and only shows the essential features to the user.
+# class Car:
+#     def __init__(self):
+#         self.acc = False
+#         self.brk = False
+#         self.clutch = False
+    
+#     def start(self):
+#         self.clutch = True
+#         self.acc = True
+#         print("car started")
+
+# car1 = Car()
+# car1.start()
+
+
+
+#Encaptulation --- Wrapping data and function into a single unit (object).
+
+class bankacc1:
+    def __init__(self, balance, acc_no):
+        self.balance = balance
+        self.acc_no = acc_no
+
+    def debit(self, amount):
+        self.balance -= amount
+        print("rs", amount , "was debited")
+        print("total balance =", self.get_balance())
+
+    def credit(self, amount):
+        self.balance += amount
+        print("rs", amount , "has been credited")
+        print("total balance =", self.get_balance())
+
+    def get_balance(self):
+        return self.balance
+
+
+acc1 = bankacc1(10000, "12345")
+acc1.debit(1000)
+acc1.credit(500) 
