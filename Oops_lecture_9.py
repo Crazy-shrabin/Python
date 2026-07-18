9a0cad29063d4c53cb652a71d79a7edd572284df
Shrabin00007 <shrabin8@gmail.com>
2026-07-18 21:53:07 +0545

    gg

--- Oops_lecture_9.py
+++ Oops_lecture_9.py
@@ -1,9 +1,40 @@
-class Student:
-    def __init__(self, name):
-        self.name = name;
+# class car:
+#     def __init__(self, type):
+#         self.type = type
 
-s1 = Student("Bibek Parajuli") 
-print(s1.name)
-del s1
-print(s1.name)
-        
+#     @staticmethod
+#     def start():
+#         print("Car has been started.")
+
+#     @staticmethod
+#     def stop():
+#         print("Car has been stopped.")
+
+# class ToyotaCar(car):
+#     def __init__(self, name, type):       
+#         super().__init__(type)
+#         self.name = name
+#         super().start()
+
+
+# car1 = ToyotaCar("fortuner", "disel")
+
+# car1.start()
+# print("the vechile type is:",car1.type)  
+
+
+
+class Person:
+    name  = "ananymous"
+
+    def changeName(self, name):
+        self.name = name
+
+p1 = Person()
+p1.changeName("rahul kunwar")
+print(p1.name)
+print(Person.name)
+
+
+
+
